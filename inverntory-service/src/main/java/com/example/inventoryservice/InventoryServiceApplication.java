32e59fae842f4d2820dd0431fcc75806af96af59
ShamilSh777 <105242215+ShamilSh777@users.noreply.github.com>
2023-11-07 00:28:27 +0400

    combining classes

---
+++ inverntory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package com.example.inventoryservice;
+
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("Iphone_13");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("phone_13_gray");
+            inventory1.setQuantity(0);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+        };
+    }
+}
---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,48 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderLineItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = Collections.emptyList(); // Initialize an empty list
+
+        if (orderRequest.getOrderLineItemsDtoList() != null) {
+            orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                    .stream()
+                    .map(this::mapToDto)
+                    .toList();
+        }
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
